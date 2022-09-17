38ff10475afd26d0057d76d069403d888bb72292
209 <209@academicos.cesde.edu.co>
2022-09-17 13:01:10 -0500

    NTS7-Se crea la linea base del proyecto

---
+++ main.py
@@ -0,0 +1,18 @@
+print("=== MENU ===")
+print("===1. SALUDAR ===")
+print("===2. DESPEDIR ===")
+print("===3. SALUDAR Y DESPEDIR ===")
+print("===4. SALIR ===")
+
+opt = 100
+
+while(opt != 4):
+    if():
+
+    elif():
+
+    elif():
+
+    elif():
+
+    else:
